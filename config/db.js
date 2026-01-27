9ac84fd1191b2a609dc971146c5d24bf4aabefe2
Diana <Diana@email.com>
2026-01-26 20:26:02 -0600

    AIVEN

--- config/db.js
+++ config/db.js
@@ -8,7 +8,7 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: 3306,
+  port: 11912,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
